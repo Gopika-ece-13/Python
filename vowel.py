6a6dd92036a709e58bb03fa07ee576dcb58ccd16
Gopika-ece-13 <neelavathigopika@gmail.com>
2026-08-26 22:00:49 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,1 +1,9 @@
 vowel
+ch = input("Enter a single character: ")
+
+if len(ch) != 1 or not ch.isalpha():
+    print("Invalid Input")
+elif ch.lower() in "aeiou":
+    print("Vowel")
+else:
+    print("Consonant")
